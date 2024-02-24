724cae7307eec1a45bb8943ccaefee779a974df3
Stalinium Network <devdeveloper433@gmail.com>
2024-02-23 22:36:12 -0800

    2

--- src/socket/io.rs
+++ src/socket/io.rs
@@ -1,17 +1,31 @@
 use axum;
-use socketioxide::{extract::SocketRef, SocketIo};
+use mongodb::bson::doc;
+use socketioxide::{extract::{SocketRef, Data}, SocketIo};
 use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
 use tracing::{error, info};
 use tracing_subscriber::FmtSubscriber;
 
+enum Res {
+    Message {
+        username: String,
+        message: String,
+    },
+}
+
 fn on_connect(socket: SocketRef) {
     info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
     socket.emit("auth", "auth").ok();
 
-    socket.on("msg", |socket: SocketRef, data: String| {
-        info!("Received event: {:?}", data);
-        socket.emit("message-back", data).ok();
+    socket.on("msg", |socket: SocketRef, Data::<String>(msg)| {
+        info!("Received event: {:?}", msg);
+
+        let msg = Res::Message {
+            username: "test".to_string(),
+            message: msg,
+        };
+
+        socket.emit("message-back", doc! {"id": "1"}).ok();
     });
 }
 
