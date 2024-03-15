9c0a147ec152beef701713c07c80073001965f2d
Stalinium Network <devdeveloper433@gmail.com>
2024-03-14 20:34:21 -0700

    save

--- src/chat/db.rs
+++ src/chat/db.rs
@@ -1,7 +1,10 @@
+extern crate chrono;
+use chrono::prelude::*;
+use crate::internal::conf::main::get_conf;
 use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 use tokio::sync::Mutex;
-use crate::internal::conf::main::get_conf;
+
 lazy_static! {
     static ref MESSAGES: Mutex<Vec<MessageItem>> =Mutex::new(Vec::new());
 }
@@ -18,7 +21,7 @@ pub async fn add_msg(id: String, msg: String) -> MessageItem {
     let msg_obj = MessageItem {
         id,
         text: msg,
-        time: "12:30".to_string(),
+        time: Utc::now().to_string(),
     };
 
     let mut messages_lock = MESSAGES.lock().await;
